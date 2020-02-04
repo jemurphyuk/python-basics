0a7b5816582a1faffc28b3fed74ed9edba79b2fb
James Murphy <jmurphy@spartaglobal.com>
2020-02-04 15:15:43 +0000

    python concatenation

---
+++ 102_data_types.py
@@ -0,0 +1,13 @@
+# Strings, characters put together in a list
+# defined by '' or ""
+
+# Joining of two strings, just add, concatenation
+first_name = 'James'
+last_name = 'Murphy'
+print (first_name + ' ' + last_name)
+full_name = (first_name + ' ' + last_name)
+print(full_name)
+
+# Interpolation, inject a string into another string
+welcome = "welcome to the danger zone"
+print (f'{full_name} welcome')
