a1024520bb6314fc9e39ff3fa4c948df90c81f1e
James Murphy <jmurphy@spartaglobal.com>
2020-02-04 15:28:47 +0000

    python string functions

--- 102_data_types.py
+++ 102_data_types.py
@@ -11,3 +11,14 @@ print(full_name)
 # Interpolation, inject a string into another string
 welcome = "welcome to the danger zone"
 print (f'{full_name} welcome')
+
+# Special methods for strings
+my_string = "   Hello, i'm amazing"
+# .count(), for specific letter or characters
+print (my_string.strip()) # remove white space before or after
+# len function not method
+print(len(my_string))
+print (my_string.upper()) # all letters caps
+print (my_string.strip().split(","))
+
+
